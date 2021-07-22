b75428afbc78d38e11446fef0622f8ee19eb4d2e
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-07-22 12:28:08 +0530

    auto commit modified on `12:28:08.58`

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,8 +1,4 @@
-@echo off
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-3 delims=/:/ " %%a in ('time /t') do (set mytime=%%a-%%b-%%c)
-set mytime=%mytime: =% 
-echo %mydate%_%mytime%
+echo %time%
 git add .
-git commit -m "modified on `%mydate%_%mytime%`"
+git commit -m "auto commit modified on `%time%`"
 git push origin master
