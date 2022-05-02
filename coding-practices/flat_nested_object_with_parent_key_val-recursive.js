a600763cbe9aabc21fcf113853f03818d2fbc5a3
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-05-02 23:33:13 +0530

    flat object and return output like parent_child_key format with recursive fn call

---
+++ coding-practices/flat_nested_object_with_parent_key_val-recursive.js
@@ -0,0 +1,36 @@
+/*
+Recursive Fn to flat object
+*/
+let user = {
+    name: 'saikat',
+    address: {
+        home: {
+            city: 'kolkata',
+            area: 'airport'
+        },
+        office: {
+            city: 'kolkata',
+            area: 'newtown'
+        }
+    }
+}
+// output
+// user_address_home_area: "airport"
+// user_address_home_city: "kolkata"
+// user_address_office_area: "newtown"
+// user_address_office_city: "kolkata"
+// user_name: "saikat"
+
+let finalObj = {};
+let flatObj = (obj, parent) => {
+    for(let key in obj) {
+        if(typeof obj[key] === 'object') {
+            flatObj(obj[key], parent + '_' + key); // recursive
+        } else {
+            finalObj[parent + '_' + key] = obj[key];
+        }
+    }
+}
+
+flatObj(user);
+console.log(finalObj);
